4b6d2f539487b73744b9576a0e3c07ff50edad5c
Sneha Biswas <sneha.a.biswas7@gmail.com>
2024-03-10 19:36:59 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app" class="wrapper">
+    <SearchView/>
+  </div>
+</template>
+
+<script>
+import SearchView from './views/SearchView.vue';
+export default {
+  name: 'App',
+  components:{
+    SearchView
+  }
+};
+</script>
+
+<style scoped>
+</style>
+
